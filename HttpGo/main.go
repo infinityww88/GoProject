14e4496905fadbd3da7e86cae3b5c78fb7ca59b7
william <1368763923@qq.com>
2023-08-16 13:06:15 +0800

    Add log in health check.

--- HttpGo/main.go
+++ HttpGo/main.go
@@ -18,14 +18,16 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	if serverIsHealthy() {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "Server is healthy\n")
+		log.Printf("health check ok\n")
 	} else {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "Server is not healthy")
+		log.Printf("health check failed\n")
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("> new request " + r.URL.Host)
+	log.Printf("new request " + r.URL.Host)
 	ret := r.URL.Query()
 	a, _ := strconv.ParseInt(ret.Get("a"), 10, 32)
 	b, _ := strconv.ParseInt(ret.Get("b"), 10, 32)
